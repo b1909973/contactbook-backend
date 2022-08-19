7d632748957819bd302c09d1423a278d8c0b3710
Nguyen Van Phuong <phuongb909973@student.ctu.edu.vn>
2022-08-19 16:55:27 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,23 @@
+
+
+exports.create = (req,res)=>{
+    res.send({message:"create handle"})
+}
+exports.findAll = (req,res)=>{
+    res.send({message:"findAll"})
+}
+exports.findOne = (req,res)=>{
+    res.send({message:"find One"})
+}
+exports.update = (req,res)=>{
+    res.send({message:"Update"})
+}
+exports.delete = (req,res)=>{
+    res.send({message:"Delete"})
+}
+exports.deleteAll  = (req,res)=>{
+    res.send({message:"DeleteAll"})
+}
+exports.findAllFavorite = (req,res)=>{
+    res.send({message:"findAllFavorite handle"})
+}
